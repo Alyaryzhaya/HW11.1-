d909c26a8f0dc99de160d441a2329813c913ea01
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-10-17 21:38:44 +0300

    tests

--- src/main/java/ru/netology/hw/Radio.java
+++ src/main/java/ru/netology/hw/Radio.java
@@ -2,59 +2,96 @@ package ru.netology.hw;
 
 public class Radio {
 
-    private int stationNumber;
+
     private int volume;
+    private int stationQuantity = 10;  // количество радиостанций
+    private int currentStatQuantity = stationQuantity;
+    private int maxStationNumber = stationQuantity - 1;
+    private int stationNumber;
+    private int currentStatNumber;
 
-    public int getStationNumber() {
-        return stationNumber;
+
+    // КОНСТРУКТОРЫ
+    public Radio(int stationQuantity) {
+        this.currentStatQuantity = stationQuantity;
+        this.maxStationNumber = stationQuantity - 1;
     }
 
-    // 1. Номер текущей радиостанции может принимать значения только в пределах от 0 до 9
-    // 4. Клиент должен иметь возможность выставлять номер радиостанции через прямое указание её номера
-    // (для этого подойдёт один метод-сеттер с проверкой на допустимость номера станции)
-    public void setStationNumber(int newStationNumber) {
-
-        if (newStationNumber < 0) {
-            return;
-        }
-        if (newStationNumber > 9) {
-            return;
-        }
-        stationNumber = newStationNumber;
+    public Radio() {
     }
 
-    // 2. Если текущая радиостанция - 9 и клиент нажал на кнопку next на пульте, то текущей должна стать 0-ая;
-    // в остальных случаях при нажатии на эту же кнопку радио переключается просто на следующую станцию.
+    //===========================================================================
+
+    public int getStationQuantity() {  // количество станций
+        return currentStatQuantity;
+    }
+
+    public int getStationNumber() { // номер станции
+        return currentStatNumber;
+    }
+
+    public int getMaxStationNumber() {
+        return maxStationNumber;
+    }
+
+    //===============================================================================
+
+
+    //2. Номер текущей радиостанции изменяется в пределах от 0 до количества радиостанций
+    // не включительно. То есть если станций 10, то номер последней — 9.
+
+    public void setStationNumberNew(int newCurrentStatNumber) {
+
+        if (newCurrentStatNumber < 0) {
+            return;
+        }
+        if (newCurrentStatNumber >= currentStatQuantity) {
+            return;
+        }
+        this.currentStatNumber = newCurrentStatNumber;
+    }
+
+
+    // 3. Если текущая радиостанция — максимальная, и клиент нажал на кнопку next, следующая, на пульте, то текущей должна стать нулевая.
     public void nextStationNumber() {
 
-        if (stationNumber == 9) {
-            stationNumber = 0;
-            // if (stationNumber >= 0 && stationNumber < 9) {
-        } else {
-            stationNumber++;
+
+        if (currentStatNumber < maxStationNumber) {
+            currentStatNumber++;
+        }else{
+            currentStatNumber = 0;
         }
+        stationNumber = currentStatNumber;
+
     }
 
-    // 3. Если текущая радиостанция - 0 и клиент нажал на кнопку prev  на пульте, то текущей должна стать 9-ая;
-    // в остальных случаях радиопереключается просто на предыдущую станцию.
+
+    // 4. Если текущая радиостанция — 0, и клиент нажал на кнопку prev, предыдущая, на пульте, то текущей должна стать максимальная.
+
 
     public void prevStationNumber() {
 
-        if (stationNumber == 0) {
-            stationNumber = 9;
+        if (currentStatNumber == 0) {
+            currentStatNumber = maxStationNumber;
+
         } else {
-            stationNumber--;
+            currentStatNumber--;
         }
+
+        stationNumber = currentStatNumber;
     }
-    //======================================================================================================================================
-    //  1. Клиент должен иметь возможность увеличивать и уменьшать уровень громкости звука (в пределах от 0 до 10)
+
+
+//======================================================================================================================================
+
+    // 1. клиент должен иметь возможность увеличивать и уменьшать уровень громкости звука в пределах от 0 до 100;
 
     public void setVolume(int newVolume) {
         if (newVolume < 0) {
             return;
         }
-        if (newVolume > 10) {
-            newVolume = 10;
+        if (newVolume > 100) {
+            newVolume = 100;
         }
         volume = newVolume;
     }
@@ -66,10 +103,10 @@ public class Radio {
     // 2.Если уровень громкости звука достиг максимального значения, то дальнейшее нажатие на +
     //не должно ни к чему приводить
     public void nextUntilMax() {
-        if (volume < 10) {
+        if (volume < 100) {
             volume++;
         } else {
-            volume = 10;
+            volume = 100;
         }
     }
 
